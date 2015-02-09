ff1ef6655b778515243bc33704354c1b25a59abb
Steeve Morin <steeve.morin@gmail.com>
2015-02-09 13:43:29 +0100

    Add packing, fix exceptions
    Signed-off-by: Steeve Morin <steeve.morin@gmail.com>

--- aml-imgpack.py
+++ aml-imgpack.py
@@ -45,6 +45,7 @@ ARCH_ARM = 8
 #     char    name[IH_NMLEN]; /* Image Name       */
 # }AmlResItemHead_t;
 
+
 class AmlResourcesImage(object):
     def __init__(self):
         self.header = AmlResImgHead()
@@ -64,27 +65,56 @@ class AmlResourcesImage(object):
         return img
 
     def pack(self):
-        self.header.imgItemNum = len(self.items)
-        self.header.imgSz = 0
-        packed = self.header.pack()
-        for item in self.items():
+        packed = ""
+        for i, item in enumerate(self.items):
+            item.index = i
+            item.start = len(packed) + AmlResImgHead._size + AmlResItem._size
+            item.dcrc = binascii.crc32(item.data) & 0xFFFFFFFF
+            if i < (len(self.items) - 1):
+                item.next = item.start + item.size
+                item.next += item.next % self.header.alignSz
             packed += item.pack()
+            packed += struct.pack("%ds" % (len(packed) % self.header.alignSz), "\0" * self.header.alignSz)
+        self.header.imgItemNum = len(self.items)
+        self.header.imgSz = len(packed) + AmlResImgHead._size
+        return self.header.pack() + packed
+
 
 class AmlResItem(object):
     _format = "IIIIIIIBBBB%ss" % IH_NMLEN
-    size = struct.calcsize(_format)
+    _size = struct.calcsize(_format)
     magic = IH_MAGIC
+    hcrc = 0
+    size = 0
+    start = 0
+    end = 0
+    next = 0
+    dcrc = 0
+    index = 0
+    nums = ARCH_ARM
+    type = 0
+    comp = 0
+    name = ""
+    data = ""
 
+    @classmethod
+    def from_file(cls, file):
+        item = cls()
+        with open(file) as fp:
+            item.data = fp.read()
+        item.size = len(item.data)
+        item.name = file.replace(".bmp", "")
+        return item
 
     @classmethod
     def unpack_from(cls, fp):
         h = cls()
         h.magic, h.hcrc, h.size, h.start, h.end, h.next, h.dcrc, h.index, \
-        h.nums, h.type, h.comp, h.name = struct.unpack(h._format, fp.read(h.size))
+        h.nums, h.type, h.comp, h.name = struct.unpack(h._format, fp.read(h._size))
         h.name = h.name.rstrip('\0')
         if h.magic != IH_MAGIC:
-            raise Exception("Invalid item header magic, should 0x%x, is 0x%x", \
-                IH_MAGIC, h.magic)
+            raise Exception("Invalid item header magic, should 0x%x, is 0x%x" % \
+                (IH_MAGIC, h.magic))
         fp.seek(h.start)
         h.data = fp.read(h.size)
         return h
@@ -98,19 +128,22 @@ class AmlResItem(object):
         return "AmlResItem(name=%s start=0x%x size=%d)" % (self.name, self.start, self.size)
 
 
-
 class AmlResImgHead(object):
     _format = "Ii%dsIII%ds" % (AML_RES_IMG_V1_MAGIC_LEN, AML_RES_IMG_HEAD_SZ - 8 * 3 - 4)
-    size = struct.calcsize(_format)
-    magic = AML_RES_IMG_V1_MAGIC
-    alignSz = AML_RES_IMG_ITEM_ALIGN_SZ
+    _size = struct.calcsize(_format)
+    crc = 0
     version = AML_RES_IMG_VERSION_V2
+    magic = AML_RES_IMG_V1_MAGIC
+    imgSz = 0
+    imgItemNum = 0
+    alignSz = AML_RES_IMG_ITEM_ALIGN_SZ
+    reserv = ""
 
     @classmethod
     def unpack_from(cls, fp):
         h = cls()
         h.crc, h.version, h.magic, h.imgSz, h.imgItemNum, h.alignSz, \
-        h.reserv = struct.unpack(h._format, fp.read(h.size))
+        h.reserv = struct.unpack(h._format, fp.read(h._size))
         if h.magic != AML_RES_IMG_V1_MAGIC:
             raise Exception("Magic is not right, should %s, is %s" % (AML_RES_IMG_V1_MAGIC, h.magic))
         if h.version > AML_RES_IMG_VERSION_V2:
@@ -136,31 +169,36 @@ def list_items(logo_img_file):
             print "    %s" % item
         img.pack()
 
+
 def unpack_image_file(logo_img_file):
     print "Unpacking assets in %s" % logo_img_file
     with open(logo_img_file) as fp:
         img = AmlResourcesImage.unpack_from(fp)
         for item in img.items:
-            print "Unpacking %s" % item.name
+            print "  Unpacking %s" % item.name
             with open("%s.bmp" % item.name, "w") as item_fp:
                 item_fp.write(item.data)
 
-def pack_image_file(assets):
+
+def pack_image_file(outfile, assets):
     img = AmlResourcesImage()
+    img.items = map(AmlResItem.from_file, assets)
+    with open(outfile, "w") as fp:
+        fp.write(img.pack())
 
 
 def main():
     parser = argparse.ArgumentParser(description='Pack and unpack amlogic uboot images')
-    parser.add_argument("--unpack", help="Unpack logo.img file", dest='unpack', action="store_true")
-    parser.add_argument("--pack", help="Unpack logo.img file", dest="pack", action="store_true")
-    parser.add_argument("--logo", help="Source/Destination logo.img file")
+    parser.add_argument("--unpack", help="Unpack logo.img file", action="store_true")
+    parser.add_argument("--pack", help="Unpack logo.img file")
+    parser.add_argument('assets', metavar='file', type=str, nargs='+', help='an integer for the accumulator')
 
     args = parser.parse_args()
     if args.unpack:
-        unpack_image_file(args.logo)
-    # elif args.pack:
-    #     pass
+        unpack_image_file(args.assets[0])
+    elif args.pack:
+        pack_image_file(args.pack, args.assets)
     else:
-        list_items(args.logo)
+        list_items(args.assets[0])
 
 main()
